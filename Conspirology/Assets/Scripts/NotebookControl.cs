bbbed838dcd0088d633f571b6078796a95c8ca65
alex23job <106354657+alex23job@users.noreply.github.com>
2025-06-07 01:35:30 +0300

    Update NotebookControl.cs

--- Conspirology/Assets/Scripts/NotebookControl.cs
+++ Conspirology/Assets/Scripts/NotebookControl.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NotebookControl : MonoBehaviour
 {
+    [SerializeField] private Button btnToLeft;
+    [SerializeField] private Button btnToRight;
+    [SerializeField] private Button[] arrBtnItems;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip clickItem;
+    [SerializeField] private AudioClip clickMerge;
+    [SerializeField] private AudioClip clickChange;
+
+    private int currentNote = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ViewItemButtons();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void ViewItemButtons()
+    {
+
+    }
+
+    public void OnBtnItemClick(int num)
+    {
+        audioSource.clip = clickItem;
+        audioSource.Play();
+    }
+
+    public void OnBtnLeftClick()
+    {
+        audioSource.clip = clickChange;
+        audioSource.Play();
+    }
+
+    public void OnBtnRightClick()
+    {
+        audioSource.clip = clickChange;
+        audioSource.Play();
+    }
+
+    public void OnBtnMergeClick(int num)
+    {
+        audioSource.clip = clickMerge;
+        audioSource.Play();
     }
 }
