1c0c21d6cbb8ed83061ef5016202f4b85ce9188d
alex23job <106354657+alex23job@users.noreply.github.com>
2025-06-07 13:27:24 +0300

    Adding NoteIem.cs

---
+++ Conspirology/Assets/Scripts/NoteItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+[Serializable]
+public class NoteItem
+{
+    private string day;
+    private string location;
+    private string nameNPC;
+    private string description;
+
+    public string Day { get => day; }
+    public string Location { get => location; }
+    public string NameNPC { get => nameNPC; }
+    public string Description { get => description; }
+
+    public NoteItem() { }
+    public NoteItem(string d, string loc, string nm, string des)
+    {
+        day = d;
+        location = loc;
+        nameNPC = nm;
+        description = des;
+    }
+
+    public NoteItem(string csv, char sep = '=')
+    {
+        string[] ar = csv.Split(sep);
+        if (ar.Length == 4)
+        {
+            day = ar[0];
+            location = ar[1];
+            nameNPC = ar[2];
+            description = ar[3];
+        }
+    }
+
+    public string ToCsvString(char sep = '=')
+    {
+        return $"{day}{sep}{location}{sep}{nameNPC}{sep}{description}{sep}";
+    }
+
+    public override string ToString()
+    {
+        return $"day:{day}  location:{location}  nameNPC:{nameNPC}  description:{description}";
+    }
+}
