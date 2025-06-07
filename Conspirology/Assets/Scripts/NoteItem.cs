13293dc0a6611a94449abf951b36ca8c535e5fae
alex23job <106354657+alex23job@users.noreply.github.com>
2025-06-07 15:05:58 +0300

    Adding class NoteBook

--- Conspirology/Assets/Scripts/NoteItem.cs
+++ Conspirology/Assets/Scripts/NoteItem.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 [Serializable]
 public class NoteItem
@@ -13,7 +15,23 @@ public class NoteItem
     public string NameNPC { get => nameNPC; }
     public string Description { get => description; }
 
+    public bool IsMerged 
+    {
+        get
+        {
+            return (description.IndexOf(')') != -1);
+        }
+    }
+
     public NoteItem() { }
+
+    public NoteItem(NoteItem item)
+    {
+        day = new string(item.Day);
+        location = new string(item.Location);
+        nameNPC = new string(item.NameNPC);
+        description = new string(item.Description);
+    }
     public NoteItem(string d, string loc, string nm, string des)
     {
         day = d;
@@ -34,6 +52,46 @@ public class NoteItem
         }
     }
 
+    public void MergeItem(NoteItem item)
+    {
+        int count = 1;
+        if (IsMerged)
+        {
+            string[] ar = description.Split(')');
+            count = ar.Length;
+        }
+        string itemDescr = item.Description;
+        if (item.IsMerged)
+        {
+            
+            int index = -1;
+            do
+            {
+                index = itemDescr.IndexOf(')', index++);
+                if (index != -1)
+                {
+                    string replStr = "";
+                    if (index > 2 && itemDescr[index - 2] == ' ') replStr = itemDescr.Substring(index - 1, 1);
+                    if (index > 3 && itemDescr[index - 3] == ' ') replStr = itemDescr.Substring(index - 2, 2);
+                    itemDescr = itemDescr.Replace(replStr, count.ToString());
+                    count++;
+                }
+            } while (index != -1);
+        }
+        else
+        {
+            itemDescr = $" {count}) {item.Day} {item.Location} {item.Description}";
+        }
+        if (IsMerged)
+        {
+            description += itemDescr;
+        }
+        else
+        {
+            description = $" {count}) {day} {location} {description} {itemDescr}";
+        }
+    }
+
     public string ToCsvString(char sep = '=')
     {
         return $"{day}{sep}{location}{sep}{nameNPC}{sep}{description}{sep}";
@@ -42,5 +100,71 @@ public class NoteItem
     public override string ToString()
     {
         return $"day:{day}  location:{location}  nameNPC:{nameNPC}  description:{description}";
+    }
+}
+
+[Serializable]
+public class NoteBook
+{
+    private List<NoteItem> noteItems = new List<NoteItem>();
+
+    public int CountItems { get { return noteItems.Count; } }
+
+    public NoteBook() { }
+
+    public NoteBook(string csv, char sep = '#', char sepItem = '=')
+    {
+        string[] ar = csv.Split(sep);
+        noteItems.Clear();
+        for(int i = 0; i < ar.Length; i++)
+        {
+            noteItems.Add(new NoteItem(ar[i], sepItem));
+        }
+    }
+
+    public void AddItem(NoteItem item)
+    {
+        noteItems.Add(item);
+    }
+
+    public void Remove(NoteItem item)
+    {
+        noteItems.Remove(item);
+    }
+
+    public void RemoveAt(int index)
+    {
+        noteItems.RemoveAt(index);
+    }
+
+    public NoteItem GetItem(int index)
+    {
+        if (index >= 0 && index < noteItems.Count)
+        {
+            return noteItems[index];
+        }
+        return null;
+    }
+
+    public void MergeItems(int numItem1, int numItem2)
+    {
+        if (numItem1 >= 0 && numItem1 < noteItems.Count)
+        {
+            if (numItem2 >= 0 && numItem2 < noteItems.Count)
+            {
+                noteItems[numItem1].MergeItem(noteItems[numItem2]);
+                noteItems.RemoveAt(numItem2);
+            }
+        }
+    }
+
+    public string ToCsvString(char sep = '#', char sepItem = '=')
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < noteItems.Count; i++)
+        {
+            sb.Append($"{noteItems[i].ToCsvString(sepItem)}{sep}");
+        }
+        return sb.ToString();
     }
 }
